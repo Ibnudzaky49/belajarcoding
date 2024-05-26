88ec1a5cc399502d74d8bf5838fbfecb31a79d39
Ibnudzaky <150795548+Ibnudzaky49@users.noreply.github.com>
2024-05-26 12:09:17 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+// let yes = document.getElementById("yes");
+
+// function ohyes(){
+//     alert("haydar asu banget")
+// }
+
+// yes.addEventListener("click", ohyes);
+
+document.getElementById("yes").addEventListener("click", function(){
+    alert("haydar asu banget")
+})
+
+document.getElementById("no").addEventListener("click", function(){
+    alert("haydar ga asu")
+})
